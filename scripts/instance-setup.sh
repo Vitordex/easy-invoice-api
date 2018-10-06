6aea306cbda47bcf0ea594d72b07aa1aff94fddd
Vitor Ribeiro Pais Del Pezzo <vitordexjesus@gmail.com>
2018-10-06 02:07:49 -0300

    feat(setup): Add useful scripts to project

--- scripts/instance-setup.sh
+++ scripts/instance-setup.sh
@@ -1,3 +1,6 @@
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -;
 sudo yum -y install nodejs;
 sudo yum -y install gcc-c++ make;
+
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+sudo python ./awslogs-agent-setup.py --region us-east-1
