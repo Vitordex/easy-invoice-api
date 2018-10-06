f96fdd2a86a51ea88b9cec409edbf3d338cba0bc
Vitor Ribeiro Pais Del Pezzo <vitordexjesus@gmail.com>
2018-10-06 11:26:19 -0300

    docs(cd): Add cloudwatch agent install to setup script

--- scripts/instance-setup.sh
+++ scripts/instance-setup.sh
@@ -2,5 +2,16 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -;
 sudo yum -y install nodejs;
 sudo yum -y install gcc-c++ make;
 
+mkdir /tmp/ssm
+curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -o /tmp/ssm/amazon-ssm-agent.rpm
+sudo yum install -y /tmp/ssm/amazon-ssm-agent.rpm
+sudo systemctl stop amazon-ssm-agent
+sudo amazon-ssm-agent -register -code "${SSM_ACTIVATION_CODE}" -id "${SSM_ACTIVATION_ID}" -region "${AWS_RES_REGION}"
+sudo systemctl start amazon-ssm-agent
+
+mkdir /var/log/${API_NAME}/
+sudo chmod +755 /var
+sudo chmod +755 /var/log
+sudo chmod +777 /var/log/${API_NAME}
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-sudo python ./awslogs-agent-setup.py --region us-east-1
+sudo python ./awslogs-agent-setup.py --region ${AWS_RES_REGION}
