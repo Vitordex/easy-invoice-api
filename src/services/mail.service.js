2d22ee810a517a4dfbfd45b61ffc219fb1405428
Vitor Ribeiro <vitordexjesus@gmail.com>
2018-10-20 17:33:02 -0300

    test(mail): Add mail service integration tests
    Issues: #71

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -1,17 +1,33 @@
 const nodemailer = require('nodemailer');
 
-class MailService{
-    constructor(transportOptions){
+class MailService {
+    constructor(transportOptions) {
         this.transporter = nodemailer.createTransport(transportOptions);
     }
 
-    sendMail(from, to, subject, body){
+    sendMail(from, to, subject, body) {
         return this.transporter.sendMail({
             from,
             to,
             subject,
             text: body
         });
+    }
+
+    async verifyConnection() {
+        const account = await nodemailer.createTestAccount();
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false,
+            auth: {
+                user: account.user,
+                pass: account.pass
+            }
+        });
+
+        return transporter.verify();
     }
 }
 
