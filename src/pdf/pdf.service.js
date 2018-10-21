5e648fab066d8a62ec47c7d4eba68e46ae2a92c4
Vitor Ribeiro <vitordexjesus@gmail.com>
2018-10-21 17:29:03 -0300

    fix(api): Add generate file method
    Issues: #86

--- src/pdf/pdf.service.js
+++ src/pdf/pdf.service.js
@@ -9,13 +9,31 @@ class PdfService{
      * Generate a pdf from an html
      * @param {String} input The html string input
      * 
-     * @returns {Promise<Buffer>}
+     * @returns {Promise<ReadableStream>}
      */
-    generate(input){
+    generateStream(input){
         const options = this.generateOptions;
 
         return new Promise((resolve, reject) => {
             html2pdf.create(input, options).toStream((err, stream) => {
+                if(err) return reject(err);
+
+                resolve(stream);
+            });
+        });
+    }
+
+    /**
+     * Generate a pdf from an html
+     * @param {String} input The html string input
+     * 
+     * @returns {Promise<ReadableStream>}
+     */
+    generateFile(input, path){
+        const options = this.generateOptions;
+
+        return new Promise((resolve, reject) => {
+            html2pdf.create(input, options).toFile(path, (err, stream) => {
                 if(err) return reject(err);
 
                 resolve(stream);
