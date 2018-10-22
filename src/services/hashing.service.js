068c0b051599c13c2e03719d8285f75a795fc08d
Vitor Ribeiro <vitordexjesus@gmail.com>
2018-10-21 23:20:20 -0300

    docs(jsdoc): Add documentation to hashing service
    Issues: #9

--- src/services/hashing.service.js
+++ src/services/hashing.service.js
@@ -3,12 +3,21 @@ const crypto = require('crypto');
 const enums = require('../enums');
 
 class HashingService {
+    /**
+     * @param {String} key The secret key to hash strings
+     * @param {String} algorithm The algorithm to hash strings
+     * @param {String} encoding The encoding of the input string
+     */
     constructor(key, algorithm = enums.HASH.SHA1, encoding = enums.HASH.ENCODING) {
         this.key = key;
         this.algorithm = algorithm;
         this.encoding = encoding;
     }
 
+    /**
+     * Hash a string in configured algorithm
+     * @param {String} text The string to hash
+     */
     createHash(text) {
         return crypto
             .createHmac(this.algorithm, this.key)
@@ -16,6 +25,11 @@ class HashingService {
             .digest(this.encoding);
     }
 
+    /**
+     * Compare an input string with a hash
+     * @param {String} text The input text
+     * @param {String} hash The hash to compare
+     */
     compare(text, hash) {
         return crypto
             .createHmac(this.algorithm, this.key)
